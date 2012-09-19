ca1c3fb119e27d31e23ffe4eaf3aa69aedbcdc56
felipe bastos <felipeabm@gmail.com>
2012-09-18 21:06:51 -0300

    Migration with class
    Initial corretion

--- views/autogenerate/migrate/create.php
+++ views/autogenerate/migrate/create.php
@@ -1,1 +1,19 @@
-<?php echo '<?php'; ?>$table = '<?php echo $className; ?>';<?php$count = count($_POST['fieldName']);?><?php for($i = 0; $i < $count; $i++): ?>$field[] = array(	'fieldName' => <?php echo $fieldName[$i]; ?>,	'fieldType' => <?php echo $fieldType[$i]; ?>,	'fieldTypeRelation' => <?php echo $fieldTypeRelation[$i]; ?>,	'fieldRelationClass' => <?php echo $fieldRelationClass[$i]; ?>,);<?php endfor; ?>
+<?='<?php'?>
+
+class <?=$className?> {
+
+	public $tableName = <?=$tablename?>
+<?php
+$count = count($_POST['fieldName']);
+?>
+<?php for($i = 0; $i < $count; $i++): ?>
+$field[] = array(
+	'fieldName' => <?php echo $fieldName[$i]; ?>,
+	'fieldType' => <?php echo $fieldType[$i]; ?>,
+	'fieldTypeRelation' => <?php echo $fieldTypeRelation[$i]; ?>,
+	'fieldRelationClass' => <?php echo $fieldRelationClass[$i]; ?>,
+);
+
+<?php endfor; ?>
+
+}
