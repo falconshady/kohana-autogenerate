459692ba53c34c67a9e7c0aacaf22eab836a63c0
felipe bastos <felipeabm@gmail.com>
2013-01-21 18:44:17 -0200

    Generate belongs hasOne e hasMany atributes
    Find e process relationship of class

--- classes/controller/autogenerate/orm.php
+++ classes/controller/autogenerate/orm.php
@@ -14,6 +14,24 @@ class Controller_Autogenerate_Orm extends Controller_Template {
 	{
 		$view = new View('autogenerate/orm/create');
 		$view->className = $_POST['className'];
+		
+		$fields = explode(' ', $_POST['migrateFields']);
+		
+		foreach($fields as $field):
+			if(strstr($field, 'belongs_to')):
+				list($name, $model) = explode(':', str_replace('belongs_to:', '', $field));
+				$view->belongs_to[$name] = array('model' => $model, 'foreign_key' => $model.'_id');
+			endif;
+			if(strstr($field, 'has_one')):
+				list($name, $model) = explode(':', str_replace('has_one:', '', $field));
+				$view->has_one[$name] = array('model' => $model, 'foreign_key' => strtolower($_POST['className']).'_id');
+			endif;
+			if(strstr($field, 'has_many')):
+				list($name, $model) = explode(':', str_replace('has_many:', '', $field));
+				$view->has_many[$name] = array('model' => $model, 'foreign_key' => strtolower($_POST['className']).'_id');
+			endif;
+		endforeach;
+		
 		$filename = APPPATH.'classes/model/'.strtolower($_POST['className']).'.php';
 		$f = fopen($filename, 'w+');
 		fwrite($f, $view->render());
