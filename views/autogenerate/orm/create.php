e8de2864501a4cd852441550e07d6acf332f4d94
felipe bastos <felipeabm@gmail.com>
2013-01-21 18:57:21 -0200

    incomplete belongs render

--- views/autogenerate/orm/create.php
+++ views/autogenerate/orm/create.php
@@ -1,1 +1,20 @@
-<?php echo '<?php'; ?>class Model_<?php echo $className; ?> extends ORM {		public $_belongs_to = array();		public $_has_one = array();		public $_has_many = array();		public $_table_name = '<?php echo strtolower(Inflector::plural($className)); ?>';	public $_primary_key = 'id';	}
+<?php echo '<?php'; ?>
+
+class Model_<?php echo $className; ?> extends ORM {
+	
+	public $_belongs_to = array(
+		<?php foreach($belongs_to as $belongs): ?>
+		$<?=$belongs['name'];?> = array(
+			'model' => '<?=$belongs['model']?>',
+		);
+		<?php endforeach; ?>
+	);
+	
+	public $_has_one = array();
+	
+	public $_has_many = array();
+	
+	public $_table_name = '<?php echo strtolower(Inflector::plural($className)); ?>';
+	public $_primary_key = 'id';
+	
+}
