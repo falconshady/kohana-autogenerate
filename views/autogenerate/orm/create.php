167417bf924820e8ce99ee8eaf367b8b28c3c115
felipe bastos <felipeabm@gmail.com>
2013-01-22 12:33:41 -0200

    added hasOne and hasMany view
    less manyToMany

--- views/autogenerate/orm/create.php
+++ views/autogenerate/orm/create.php
@@ -3,16 +3,31 @@
 class Model_<?php echo $className; ?> extends ORM {
 	
 	public $_belongs_to = array(
-		<?php foreach($belongs_to as $belongs): ?>
-		$<?=$belongs['name'];?> = array(
+		<?php foreach($belongs_to as $name => $belongs): ?>
+		'<?=$name?>' = array(
 			'model' => '<?=$belongs['model']?>',
-		);
+			'foreign_key' => '<?=strtolower($belongs['model'])?>_id',
+		),
 		<?php endforeach; ?>
 	);
 	
-	public $_has_one = array();
+	public $_has_one = array(
+		<?php foreach($has_one as $name => $one): ?>
+		'<?=$name?>' = array(
+			'model' => '<?=$one['model']?>',
+			'foreign_key' => '<?=strtolower($className)?>_id',
+		),
+		<?php endforeach; ?>
+	);
 	
-	public $_has_many = array();
+	public $_has_many = array(
+		<?php foreach($has_many as $name => $many): ?>
+		'<?=$name?>' = array(
+			'model' => '<?=$many['model']?>',
+			'foreign_key' => '<?=strtolower($className)?>_id',
+		),
+		<?php endforeach; ?>
+	);
 	
 	public $_table_name = '<?php echo strtolower(Inflector::plural($className)); ?>';
 	public $_primary_key = 'id';
