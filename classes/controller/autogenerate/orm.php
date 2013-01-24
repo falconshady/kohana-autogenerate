424d9da0f5037355f73ff17d8e3b42d7bf7e36f4
felipe bastos <felipeabm@gmail.com>
2013-01-23 23:15:10 -0200

    foreign key of belongs lowcase

--- classes/controller/autogenerate/orm.php
+++ classes/controller/autogenerate/orm.php
@@ -25,7 +25,7 @@ class Controller_Autogenerate_Orm extends Controller_Template {
 		foreach($fields as $field):
 			if(strstr($field, 'belongs_to')):
 				list($name, $model) = explode(':', str_replace('belongs_to:', '', $field));
-				$view->belongs_to[$name] = array('model' => $model, 'foreign_key' => $model.'_id');
+				$view->belongs_to[$name] = array('model' => $model, 'foreign_key' => strtolower($model).'_id');
 			endif;
 			if(strstr($field, 'has_one')):
 				list($name, $model) = explode(':', str_replace('has_one:', '', $field));
