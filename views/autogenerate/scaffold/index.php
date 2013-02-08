d2a2b0ca368d2dd255f40b826fa17a7fef4609c2
felipe bastos <felipeabm@gmail.com>
2013-02-07 23:37:51 -0200

    include table e fields attributes

--- views/autogenerate/scaffold/index.php
+++ views/autogenerate/scaffold/index.php
@@ -1,1 +1,14 @@
-<?php $view = new View('autogenerate/menu'); echo $view;?><div class="span10">	<h1>AutoGenerate Scaffold</h1>	<?php echo Form::open('autogenerate/scaffold/create'); ?>	<label>Nome da Classe:</label>	<input type="text" name="className" />	<p><input type="submit" name="create" value="Criar" class="btn btn-success" /></p>	<?php echo Form::close(); ?></div>
+<?php $view = new View('autogenerate/menu'); echo $view;?>
+<div class="span10">
+	<h1>AutoGenerate Scaffold</h1>
+	<?php echo Form::open('autogenerate/scaffold/create'); ?>
+	<label>Nome da Classe:</label>
+	<input type="text" name="className" />
+	<label>Nome da Tabela:</label>
+	<input type="text" name="tableName" />
+	<p>Exemplo: users</p>
+	<label>Campos:</label>
+	<textarea name="migrateFields" rows="5" class="span12"></textarea>
+	<p><input type="submit" name="create" value="Criar" class="btn btn-success" /></p>
+	<?php echo Form::close(); ?>
+</div>
