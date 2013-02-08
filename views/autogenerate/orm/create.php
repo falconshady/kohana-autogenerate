5e73fddbf056b220d19a3fe7800cf569f22531f1
felipe bastos <felipeabm@gmail.com>
2013-02-08 01:19:34 -0200

    model name in kohana 3.2 e 3.3 versions

--- views/autogenerate/orm/create.php
+++ views/autogenerate/orm/create.php
@@ -5,25 +5,27 @@ class Model_<?=$className?> extends ORM {
 	public $_belongs_to = array(
 		<?php foreach($belongs_to as $name => $belongs): ?>
 		'<?=$name?>' => array(
-			'model' => '<?=$belongs['model']?>',
+			'model' => '<?= Kohana_Core::VERSION < 3.3 ? strtolower($belongs['model']) : $belongs['model']; ?>',
 			'foreign_key' => '<?=$belongs['foreign_key']?>',
 		),
-		<?php endforeach; ?>
+		<?php endforeach;
+		?>
 	);
 	
 	public $_has_one = array(
 		<?php foreach($has_one as $name => $one): ?>
 		'<?=$name?>' => array(
-			'model' => '<?=$one['model']?>',
+			'model' => '<?= Kohana_Core::VERSION < 3.3 ? strtolower($one['model']) : $one['model']; ?>',
 			'foreign_key' => '<?=$one['foreign_key']?>',
 		),
-		<?php endforeach; ?>
+		<?php endforeach;
+		?>
 	);
 	
 	public $_has_many = array(
 		<?php foreach($has_many as $name => $many): ?>
 		'<?=$name?>' => array(
-			'model' => '<?=$many['model']?>',
+			'model' => '<?= Kohana_Core::VERSION < 3.3 ? strtolower($many['model']) : $many['model']; ?>',
 			<?php if(isset($many['foreign_key'])):?>
 			'foreign_key' => '<?=$many['foreign_key']?>',
 			<?php endif; ?>
@@ -31,7 +33,8 @@ class Model_<?=$className?> extends ORM {
 			'through' => '<?=$many['table']?>',
 			<?php endif; ?>
 		),
-		<?php endforeach; ?>
+		<?php endforeach;
+		?>
 	);
 	
 	public $_table_name = '<?=$tableName?>';
