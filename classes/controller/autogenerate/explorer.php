6b49f832c373819d048ecde6f0f3555594b148ce
felipe bastos <felipeabm@gmail.com>
2013-09-05 19:58:17 -0300

    Update explorer.php

--- classes/controller/autogenerate/explorer.php
+++ classes/controller/autogenerate/explorer.php
@@ -1,17 +1,22 @@
 <?php
 
-class ControllerAuto_Autogenerate_Explorer extends Controller_Template {
+class Controller_Autogenerate_Explorer extends Controller_Template {
 
 	public $template = 'autogenerate/index';
 	
 	public function action_index()
 	{
-	
+		$view = View::factory('autogenerate/explorer/index');
+		$this->template->content = $view;
 	}
 	
 	public function action_create()
 	{
-		
+		if($this->request->post())
+		{
+			$filename = $this->request->post('filename');
+			//TODO: create file
+		}
 	}
 	
 	public function action_open()
