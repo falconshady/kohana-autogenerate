3d1b68485f533e948685cd25830dddfed03d28f1
felipe bastos <felipeabm@gmail.com>
2013-10-31 02:23:03 -0200

    Update view.php

--- classes/controller/autogenerate/view.php
+++ classes/controller/autogenerate/view.php
@@ -14,12 +14,13 @@ class Controller_Autogenerate_View extends Controller_Template {
 	{
 		$this->auto_render = FALSE;
 		
+		//create directory
 		$directory = str_replace('\\', '/', APPPATH.'views/'.strtolower($_POST['className']).'/');
 		if(! file_exists($directory)):
 			mkdir($directory);
 		endif;
 		
-		
+		//create index view
 		$view = new View('autogenerate/view/create_index');
 		$view->className = $_POST['className'];
 		$filename = str_replace('\\', '/', APPPATH.'views/'.strtolower($_POST['className']).'/index.php');
@@ -27,13 +28,15 @@ class Controller_Autogenerate_View extends Controller_Template {
 		fwrite($f, $view->render());
 		fclose($f);
 		
+		//create find view
 		$view = new View('autogenerate/view/create_find');
 		$view->className = $_POST['className'];
-		$filename = str_replace('\\', '/', APPPATH.'views/'.strtolower($_POST['className']).'/show.php');
+		$filename = str_replace('\\', '/', APPPATH.'views/'.strtolower($_POST['className']).'/find.php');
 		$f = fopen($filename, 'w+');
 		fwrite($f, $view->render());
 		fclose($f);
 		
+		//create show view
 		$view = new View('autogenerate/view/create_show');
 		$view->className = $_POST['className'];
 		$filename = str_replace('\\', '/', APPPATH.'views/'.strtolower($_POST['className']).'/show.php');
@@ -41,6 +44,7 @@ class Controller_Autogenerate_View extends Controller_Template {
 		fwrite($f, $view->render());
 		fclose($f);
 		
+		//crate create view
 		$view = new View('autogenerate/view/create_create');
 		$view->className = $_POST['className'];
 		$filename = APPPATH.'views/'.strtolower($_POST['className']).'/create.php';
@@ -48,6 +52,7 @@ class Controller_Autogenerate_View extends Controller_Template {
 		fwrite($f, $view->render());
 		fclose($f);
 		
+		//create update view
 		$view = new View('autogenerate/view/create_update');
 		$view->className = $_POST['className'];
 		$filename = APPPATH.'views/'.strtolower($_POST['className']).'/update.php';
@@ -55,6 +60,7 @@ class Controller_Autogenerate_View extends Controller_Template {
 		fwrite($f, $view->render());
 		fclose($f);
 		
+		//create delete view
 		$view = new View('autogenerate/view/create_delete');
 		$view->className = $_POST['className'];
 		$filename = APPPATH.'views/'.strtolower($_POST['className']).'/delete.php';
