5351ec213ca7e12b00f28b403d19d28eb107008f
felipe bastos <felipeabm@gmail.com>
2013-10-31 02:12:15 -0200

    Update create_new.php

--- views/autogenerate/view/create_new.php
+++ views/autogenerate/view/create_new.php
@@ -1,1 +1,43 @@
-<h1>New <?php echo $className; ?></h1><?php echo '<?php echo '; ?>Form::open('<?php echo strtolower($className); ?>/create');<?php echo '?>'; ?><?php/**foreach($fields as $field): ?><label><?php echo $field->name; ?></label><?phpswitch($field->type){	case 'references' :	{	?>	<select name="">	<?php echo '<?php '; ?>foreach(): <?php echo '?>'; ?>	<option value="<?php echo '<?php ','', '?>'; ?>"><?php echo '<?php ','', '?>'; ?></option>	<?php echo '<?php '; ?>endforeach; <?php echo '?>'; ?>	</select>	<?php	} break;		case 'text' : {	?>	<textarea name="<?php echo '<?php ','', '?>'; ?>"><?php echo '<?php ','', '?>'; ?></textarea>	<?php	} break;		case 'string' :	case 'integer' :	case 'decimal' :	case 'date' :	case 'datetime' :	case 'timestamp' :	default: {		echo '<input type="text" name="'.$className.'['.$field->name.']" />';	}}echo $input;?><?php endforeach;/**/?><input type="submit" name="create" value="Criar" class="btn btn-primary" /> &nbsp; <?php echo '<?php echo Html::anchor("'.strtolower($className).'/index", "Back", array("class" => "btn")); ?>'); ?><?php echo '<?php echo '; ?>Form::close();<?php echo '?>'; ?>
+<h1>New <?php echo $className; ?></h1>
+<?php echo '<?php echo '; ?>Form::open('<?php echo strtolower($className); ?>/create');<?php echo '?>'; ?>
+<?php
+/**
+foreach($fields as $field): ?>
+<label><?php echo $field->name; ?></label>
+<?php
+switch($field->type)
+{
+	case 'references' :	{
+	?>
+	<select name="">
+	<?php echo '<?php '; ?>foreach(): <?php echo '?>'; ?>
+	<option value="<?php echo '<?php ','', '?>'; ?>"><?php echo '<?php ','', '?>'; ?></option>
+	<?php echo '<?php '; ?>endforeach; <?php echo '?>'; ?>
+	</select>
+	<?php
+	} break;
+	
+	case 'text' : {
+	?>
+	<textarea name="<?php echo '<?php ','', '?>'; ?>"><?php echo '<?php ','', '?>'; ?></textarea>
+	<?php
+	} break;
+	
+	case 'string' :
+	case 'integer' :
+	case 'decimal' :
+	case 'date' :
+	case 'datetime' :
+	case 'timestamp' :
+	default: {
+		echo '<input type="text" name="'.$className.'['.$field->name.']" />';
+	}
+}
+echo $input;
+?>
+
+<?php endforeach;
+/**/
+?>
+<input type="submit" name="create" value="Criar" class="btn btn-primary" /> &nbsp; <?php echo '<?php echo Html::anchor("'.strtolower($className).'/index", "Back", array("class" => "btn")); ?>'; ?>
+<?php echo '<?php echo '; ?>Form::close();<?php echo '?>'; ?>
